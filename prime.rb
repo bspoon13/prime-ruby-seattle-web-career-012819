055a7b1d3390deab8be192ff0708645a58b2020f
Billy Witherspoon <wwither@gmail.com>
2019-01-18 02:52:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(int)
+  divisibles = Array (2...int)
+  if int < 2
+    return false
+  elsif int == 2
+    return true
+  else
+    divisibles.each do |number|
+      if int % number == 0
+        return false
+        break
+      elsif number == int - 1
+        return true
+      else
+      end
+    end
+  end
+end
